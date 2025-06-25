2ca179f683b4fd69c571748f807c58320fff245d
chris samuel <112623896+chrisracha@users.noreply.github.com>
2025-06-25 15:05:03 +0800

    Add project files.

---
+++ ExpenseTrackerApp/ExpenseTrackerApp/Data/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTrackerApp.Data
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string ICollection<Expense> Expenses { get; set; }
+
+    }
+}
---
+++ ExpenseTrackerApp/ExpenseTrackerApp/Data/Expense.cs
@@ -0,0 +1,15 @@
+﻿namespace ExpenseTrackerApp.Data
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; }
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsIncome { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public string? UserId { get; set; }
+
+    }
+}
